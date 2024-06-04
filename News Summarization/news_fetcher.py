ebb690aaf2eee6c39c6f53b99fde73fc5078411a
varshi-05 <157141769+varshi-05@users.noreply.github.com>
2024-06-04 23:11:31 +0530

    Add files via upload

---
+++ News Summarization/news_fetcher.py
@@ -0,0 +1,49 @@
+import requests
+
+import streamlit as st
+from newsapi import NewsApiClient
+
+
+class NewsFetcher(object):
+    """
+    A wrapper for extracting news for a 
+    queried entity from the NewsAPI client.
+
+    Attributes:
+        query (str): user input 
+        api (NewsAPIClient): the instantiated client 
+
+    See https://github.com/mattlisiv/newsapi-python for
+    further information on the Python client library.
+    """
+
+    API_KEY = 'your_key' # place your API key here once downloaded from https://newsapi.org
+
+    def __init__(self, query):
+        self.query = query  # e.g., 'Donald Trump' 
+        self.api = NewsApiClient(api_key=NewsFetcher.API_KEY)
+    
+
+    def get_everything(self):
+        """
+        Extracts meta-data for news articles, given
+        a query passed from the user.  
+        
+        See https://newsapi.org/docs/endpoints/everything
+        for request parameters and response fields. 
+
+        Returns:
+            news (NewsAPI): JSON response object 
+        """
+        from datetime import datetime
+        #today = datetime.today().strftime('%Y-%m-%d')
+        try:
+            news = self.api.get_everything(q=self.query, # 'everything' endpoint
+                                    language='en',
+                                    page_size=5, # restricted due to the load of performing BART inference
+                                    sort_by='relevancy') 
+            return news
+        except requests.exceptions.ConnectionError:
+            pass
+       
+
